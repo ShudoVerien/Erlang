d866f85508a851557a947288c0d0553ce83c73fe
ILYA STRILTSIV <73172987+ShudoVerien@users.noreply.github.com>
2024-12-12 17:34:51 +0200

    Update lesson2_task06.erl

--- lesson2_task06.erl
+++ lesson2_task06.erl
@@ -1,4 +1,9 @@
 -module(lesson2_task06).
 -export([is_palindrome/1]).
 
-is_palindrome(List) -> List == lists:reverse(List).
+is_palindrome(List) -> List == reverse(List).
+
+reverse(List) -> reverse(List, []).
+
+reverse([], Acc) -> Acc;
+reverse([H|T], Acc) -> reverse(T, [H | Acc]).
